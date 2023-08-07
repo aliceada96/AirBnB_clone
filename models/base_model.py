658744583d1f7ef81e8e02c40891b20eec0e5d8c
aliceada96 <aliceada96@gmail.com>
2023-08-08 00:07:48 +0300

    Add test cases for base_model, task 3

--- models/base_model.py
+++ models/base_model.py
@@ -7,8 +7,7 @@ import datetime
 
 class BaseModel:
     def __init__(self):
-        """Initialize an instance of the BaseModel.
-        """
+        """Initialize an instance of the BaseModel."""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = self.created_at
@@ -16,7 +15,7 @@ class BaseModel:
     def __str__(self):
         """Print : [<class name>] (<self.id>) <self.__dict__>;"""
         return "[{}] ({}) {}".format(
-             self.__class__.__name__, self.id, vars(self))
+            self.__class__.__name__, self.id, vars(self))
 
     def save(self):
         """Update the public instance attribute updated_at
@@ -25,9 +24,9 @@ class BaseModel:
 
     def to_dict(self):
         """Return a dictionary containing all keys/values
-         of __dict__ of the instance:
+        of __dict__ of the instance:
         """
-        self_dict = self.__dict__
+        self_dict = self.__dict__.copy()
         self_dict["__class__"] = self.__class__.__name__
         self_dict["created_at"] = self_dict["created_at"].isoformat()
         self_dict["updated_at"] = self_dict["updated_at"].isoformat()
